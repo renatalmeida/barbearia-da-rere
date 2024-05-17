0b96670d06207d1e403031898bf96156708affaa
renatalmeida <166084156+renatalmeida@users.noreply.github.com>
2024-05-17 01:13:08 +0000

    16/05/24

---
+++ script.js
@@ -0,0 +1,30 @@
+text-align: center;
+}
+
+em strong{
+  color: red;
+}
+
+#missao{
+  font-size: 28px;
+}
+
+.beneficios{
+  padding: 20px;
+  background-color: #FFFFFF;
+}
+
+ul{
+  display: inline-block;
+  vertical-align: top;
+  width: 20%;
+  margin-right: 15%;
+}
+
+.item{
+  font-style: italic;
+}
+
+.imagem-beneficios{
+  width: 50%;
+}
